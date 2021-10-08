6da8d5d47b0392efe8cb76d9361215ee825881d3
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-10-08 16:46:50 +0200

    section 2 terminé : les routes

--- expressmovies/app.js
+++ expressmovies/app.js
@@ -3,10 +3,24 @@ const app = express();
 
 const PORT = 3000 ; 
 
-app.get('/',function(req, res){
+app.get('/movies', (req, res) =>{
+    res.send('Bientôt des films ici même !');
+});
+
+app.get('/movies/add', (req, res) => {
+    res.send(`Prochainement un formulaire d\ 'ajout ici`);
+});
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`film numéro ${id}`);
+});
+
+
+app.get('/', (req, res) => {
     res.send('Hello <b>World !!!!!</b>');
 });
 
-app.listen(PORT , function() {
+app.listen(PORT , () => {
  console.log(`listening on port ${PORT}`);
 });
