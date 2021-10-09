eebecb4b7e88eaea7b4828362bf69b27695244ed
Zidiny Erre <mr.erre.zidiny@gmail.com>
2021-10-09 21:06:19 +0200

    problème avec ejs

--- expressmovies/app.js
+++ expressmovies/app.js
@@ -3,9 +3,19 @@ const app = express();
 
 const PORT = 3000 ; 
 
+app.use( express.static(__dirname+"public"));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/movies', (req, res) =>{
-    res.send('Bientôt des films ici même !');
+    // res.send('Bientôt des films ici même !');
+    res.render('movies');
+
 });
+
+app.get('/movies-details', (req, res) => {
+    res.render('movies-details');
+})
 
 app.get('/movies/add', (req, res) => {
     res.send(`Prochainement un formulaire d\ 'ajout ici`);
@@ -13,12 +23,14 @@ app.get('/movies/add', (req, res) => {
 
 app.get('/movies/:id', (req, res) => {
     const id = req.params.id;
-    res.send(`film numéro ${id}`);
+    // res.send(`film numéro ${id}`);
+    res.render('movie-details', {movieid = id });
 });
 
 
-app.get('/', (req, res) => {
-    res.send('Hello <b>World !!!!!</b>');
+app.get('/', (req, res) => { 
+    // res.send('Hello <b>World !!!!!</b>');
+    res.render('index');
 });
 
 app.listen(PORT , () => {
